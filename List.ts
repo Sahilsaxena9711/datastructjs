c821febe0bd61ff83c304f8e6678010db6a96b5f
Sahilsaxena9711 <sahilsaxena9711@gmail.com>
2025-02-21 23:16:17 +0530

    [added getSize() to list]

--- List.ts
+++ List.ts
@@ -6,16 +6,20 @@ class List {
     this.value = list;
   }
 
-  add(el: any = undefined) {
+  add(el: any = undefined): any[] {
     this.value = [...this.value, el];
     this.size = this.value.length;
     return this.value;
   }
 
-  remove(el: any = undefined) {
+  remove(el: any = undefined): any[] {
     this.value = this.value.filter((val) => val != el);
     this.size = this.value.length;
     return this.value;
+  }
+
+  getSize(): number {
+    return this.size;
   }
 }
 
